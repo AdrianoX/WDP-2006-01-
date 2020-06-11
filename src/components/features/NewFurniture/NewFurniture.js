0b3d712c8f5221311a698d3236378613f7ea2075
Kamil Bogucki <boguckikamil@hotmail.com>
2020-06-11 20:38:00 +0200

    add RWD to new furniture section

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -44,10 +44,10 @@ class NewFurniture extends React.Component {
         <div className='container'>
           <div className={styles.panelBar}>
             <div className='row no-gutters align-items-end'>
-              <div className={'col-auto ' + styles.heading}>
+              <div className={styles.heading}>
                 <h3>New furniture</h3>
               </div>
-              <div className={'col ' + styles.menu}>
+              <div className={'col-sm-12 ' + styles.menu}>
                 <ul>
                   {categories.map(item => (
                     <li key={item.id}>
@@ -61,14 +61,14 @@ class NewFurniture extends React.Component {
                   ))}
                 </ul>
               </div>
-              <div className={'col-auto ' + styles.dots}>
+              <div className={styles.dots}>
                 <ul>{dots}</ul>
               </div>
             </div>
           </div>
           <div className='row'>
             {categoryProducts.slice(activePage * 8, (activePage + 1) * 8).map(item => (
-              <div key={item.id} className='col-3'>
+              <div key={item.id} className='col-lg-3 col-md-6 col-sm-12'>
                 <ProductBox {...item} />
               </div>
             ))}
