d9e728c20de82e64dcc4422cb85c13989e1f5947
Sergiusz Pu <serson85@gmail.com>
2020-06-14 20:13:22 +0200

    add Shop and Products Routing

---
+++ src/components/views/Products/Products.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Products.module.scss';
+
+const Products = () => (
+  <div className={styles.component}>
+    <p>A products page will be built here soon........</p>
+  </div>
+);
+
+export default Products;
---
+++ src/components/views/Shop/Shop.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Shop.module.scss';
+
+const Shop = () => (
+  <div className={styles.component}>
+    <p>A shop will be built here soon........</p>
+  </div>
+);
+
+export default Shop;
