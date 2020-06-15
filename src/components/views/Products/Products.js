a8d207fe64958f14cfc89b83dea5cbeadc46b64f
Sergiusz Pu <serson85@gmail.com>
2020-06-15 10:55:57 +0200

    change text in Shop and Products component

--- src/components/views/Products/Products.js
+++ src/components/views/Products/Products.js
@@ -3,7 +3,7 @@ import styles from './Products.module.scss';
 
 const Products = () => (
   <div className={styles.component}>
-    <p>A products page will be built here soon........</p>
+    <p>Products</p>
   </div>
 );
 
--- src/components/views/Shop/Shop.js
+++ src/components/views/Shop/Shop.js
@@ -3,7 +3,7 @@ import styles from './Shop.module.scss';
 
 const Shop = () => (
   <div className={styles.component}>
-    <p>A shop will be built here soon........</p>
+    <p>Shop</p>
   </div>
 );
 
