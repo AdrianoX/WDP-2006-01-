772b9875f1d05ca39ac70f2b265afd15f0ea46f5
Sergiusz Pu <serson85@gmail.com>
2020-06-24 16:11:50 +0200

    add compareProducts v.06

--- src/components/features/StickyBar/StickyBar.js
+++ src/components/features/StickyBar/StickyBar.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import styles from './StickyBar.module.scss';
 import PropTypes from 'prop-types';
-
+import Button from '../../common/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons';
 class StickyBar extends React.Component {
   render() {
     const { compareProduct, removeCompare } = this.props;
 
     return (
-      <div className={styles.wrapper}>
-        {compareProduct.map(product => (
-          <div key={product} className={styles.image}>
+      <div className={compareProduct.length <= 0 ? styles.dontshow : styles.wrapper}>
+        {compareProduct.map((product, index) => (
+          <div key={index} className={styles.image}>
             <img src={product.bgImageUrl} alt={product.id}></img>
             <span className={styles.close} onClick={() => removeCompare(product)}>
               X
             </span>
+            <Button className={styles.button}>
+              <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
+            </Button>
           </div>
         ))}
       </div>
