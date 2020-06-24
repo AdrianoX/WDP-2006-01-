42f4054216808ddbffad1c195b43f0c5925695d6
Marta Majerowska <majeranek85@gmail.com>
2020-06-24 21:18:07 +0200

    Delete border from columns

--- src/components/views/ProductList/ProductList.js
+++ src/components/views/ProductList/ProductList.js
@@ -6,8 +6,8 @@ const ProductList = () => (
   <div className={styles.root}>
     <div className={'container'}>
       <div className={'row'}>
-        <div className={'col-sm-12 col-md-9 border ' + styles.left}>Lewa kolumna</div>
-        <div className={'col-sm-12 col-md-3 border ' + styles.right}>Prawa kolumna</div>
+        <div className={'col-sm-12 col-md-9 ' + styles.left}>Lewa kolumna</div>
+        <div className={'col-sm-12 col-md-3 ' + styles.right}>Prawa kolumna</div>
       </div>
     </div>
   </div>
