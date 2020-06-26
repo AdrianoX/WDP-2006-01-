d1873f406e7f530a68f605358fcb8f37232daa22
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-06-27 01:20:56 +0200

    [WDP200601-52] SwipeAbleWrapper - adapting the appearance

--- src/components/features/NewFurniture/NewFurniture.js
+++ src/components/features/NewFurniture/NewFurniture.js
@@ -96,6 +96,7 @@ class NewFurniture extends React.Component {
                 }
               >
                 <h3>New furniture</h3>
+
               </div>
               <div className={'col-sm-12 ' + styles.menu}>
                 <ul>
@@ -114,6 +115,7 @@ class NewFurniture extends React.Component {
                   ))}
                 </ul>
               </div>
+              {/* <div className={styles.dots}><ul>{dots}</ul></div> */}
             </div>
           </div>
           <SwipeAbleWrapper>{changeNewPages()}</SwipeAbleWrapper>
