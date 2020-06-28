14363d8e4ada46bb5b68f525d83631ce90b984ca
Patryk Buła <patryk@bulczan.pl>
2020-06-28 19:29:51 +0200

    fix for menu

--- src/components/common/NavBar/NavBar.js
+++ src/components/common/NavBar/NavBar.js
@@ -1,23 +1,22 @@
 import React from 'react';
-// import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import styles from './NavBar.module.scss';
 
 const NavBar = () => (
-  <div>test</div>
-  // <Navbar className={styles.navbar} expand="md">
-  //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  //   <Navbar.Collapse id="basic-navbar-nav">
-  //     <Nav className="mr-auto">
-  //       <Nav.Link href="#home">Home</Nav.Link>
-  //       <Nav.Link href="#Furniture">Furniture</Nav.Link>
-  //       <Nav.Link href="#Chair">Chair</Nav.Link>
-  //       <Nav.Link href="#Table">Table</Nav.Link>
-  //       <Nav.Link href="#Sofa">Sofa</Nav.Link>
-  //       <Nav.Link href="#Bedroom">Bedroom</Nav.Link>
-  //       <Nav.Link href="#Blog">Blog</Nav.Link>
-  //     </Nav>
-  //   </Navbar.Collapse>
-  // </Navbar>
+  <Navbar className={styles.navbar} expand="md">
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#Furniture">Furniture</Nav.Link>
+        <Nav.Link href="#Chair">Chair</Nav.Link>
+        <Nav.Link href="#Table">Table</Nav.Link>
+        <Nav.Link href="#Sofa">Sofa</Nav.Link>
+        <Nav.Link href="#Bedroom">Bedroom</Nav.Link>
+        <Nav.Link href="#Blog">Blog</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 export default NavBar;
