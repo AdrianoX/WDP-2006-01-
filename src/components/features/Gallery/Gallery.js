44a22b947a60d7c95c68cd9d19a1886f57645622
Kamil Bogucki <boguckikamil@hotmail.com>
2020-06-30 20:10:03 +0200

    rebase to master

--- src/components/features/Gallery/Gallery.js
+++ src/components/features/Gallery/Gallery.js
@@ -22,18 +22,7 @@ class Gallery extends Component {
   tabChange = (newTab, e) => {
     e.preventDefault();
 
-<<<<<<< HEAD
     this.setState({ fade: true });
-=======
-    if (viewport === 'desktop') {
-      productsOnPage = 8;
-    } else if (viewport === 'tablet') {
-      productsOnPage = 4;
-    } else {
-      productsOnPage = 1;
-    }
->>>>>>> commit before rebase
-
     setTimeout(() => {
       this.setState({ activeTab: newTab });
       const subcategoryProducts = this.props.products.filter(
